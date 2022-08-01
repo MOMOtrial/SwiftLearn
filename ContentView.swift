82c2212a2dcca5ba44f016a91f84daa3ae36890f
MichaelMOMO <109764421+MichaelMOMO@users.noreply.github.com>
2022-08-01 15:56:31 -0400

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  ResConnect
+//
+//  Created by Michael Moser on 2022-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .padding(.trailing, 0.0)
+                    .font(.title)
+                .foregroundColor(.red)
+                HStack {
+                    Text("Josh Tree National Park")
+                        .foregroundColor(.blue)
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
